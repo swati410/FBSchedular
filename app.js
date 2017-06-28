ab467149efc54cd85aeafd8dd49341960f13c8a2
swatisinha <ssinha47@in.ibm.com>
2017-06-29 00:28:09 +0530

    Adding login.html file for getting auth_code and modified app.js file to post data on facebook

--- app.js
+++ app.js
@@ -1,52 +1,50 @@
 var express = require('express');
-//const https = require('https');
+var request = require('request');
 var app = express();
 var path = require('path');
 var FB = require('fb');
-var accesstoken;
-
-
-// https.get('https://graph.facebook.com/oauth/access_token?client_id=1661598120549272&client_secret=8d57b232889eb9a213f512ab404f0c16&grant_type=client_credentials', (res) => {
-//     console.log('statusCode:', res);
-//
-//
-// res.on('data', (d) => {
-//     //process.stdout.write(d);
-// });
-//
-// }).on('error', (e) => {
-//     console.error(e);
-// });
-
-FB.api('oauth/access_token', {
-    client_id: '1661598120549272',
-    client_secret: '8d57b232889eb9a213f512ab404f0c16',
-    grant_type: 'client_credentials'
-}, function (res) {
-    if(!res || res.error) {
-        console.log(!res ? 'error occurred' : res.error);
-        return;
-    }
-    console.log(JSON.stringify(res));
-    accesstoken = res.access_token;
-    //console.log(accesstoken);
-
-    FB.setAccessToken('EAACEdEose0cBANFrpxKYRje9N0vOpXSUdAgWoZBYjBpcehVQr5wmfrETnmtbsIGJjolTZCmJDpcio28pXxa4mPeipEPsZAwKEZAEzcR5wEuyfkCmGTwnYk7mD0mZBUgZAv3082m4yZAzh8nBdsq0CqXZCgoP3b97jHfbXC2TrZAZA4lJeyebuZBzvOxJkcmlppABGgZD');
-    //var accessToken = FB.getAccessToken();
-
-
-    var body = 'My first post using facebook-node-sdk';
-    FB.api('me/feed', 'post', { message: body }, function (res) {
-        if(!res || res.error) {
-            console.log(!res ? 'error occurred' : res.error);
-            return;
-        }
-        console.log('Post Id: ' + res.id);
-    });
+var auth_code;
+var access_token;
+var PostID;
 
 
 
-    //console.log("new" + accessToken);
+app.get('/auth/facebook/callback', function (req, res) {
+
+    auth_code = req.query.code
+    console.log(req);
+    res.send('Recieved Code');
+    request('https://graph.facebook.com/v2.9/oauth/access_token?client_id=1661598120549272&redirect_uri=http://localhost:8080/auth/facebook/callback&client_secret=8d57b232889eb9a213f512ab404f0c16&code='+auth_code,
+        function (error, response, body) {
+            if (!error ) {
+                var newbody = JSON.parse(body);
+                access_token = newbody.access_token
+                console.log("Access  " + newbody.access_token);
+                FB.setAccessToken(access_token);
+                var body = 'My first post using facebook-node-sdk';
+                FB.api('me/feed', 'post', { message: body }, function (res) {
+                    if(!res || res.error) {
+                        console.log(!res ? 'error occurred' : res.error);
+                        return;
+                    }
+                    console.log('Post Id: ' + res.id);
+                    //res.send(res.id);
+                });
+            }
+        });
+})
+
+app.get('/schedular', function(request, response){
+    response.sendFile(path.join(__dirname +'/fbpost.html'));
 });
 
+app.get('/login', function(request, response){
+    response.sendFile(path.join(__dirname +'/login.html'));
+});
 
+var server = app.listen(8080, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    //console.log("Example app listening at http://%s:%s", host, port)
+})
