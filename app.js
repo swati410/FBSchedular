dda528500eb3de94d376015e2f4fd10295857eb7
swatisinha <ssinha47@in.ibm.com>
2017-07-01 17:05:49 +0530

    Updating app.js and login.html

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ var path = require('path');
 var FB = require('fb');
 var auth_code;
 var access_token;
-var PostID;
 
 
 
 app.get('/auth/facebook/callback', function (req, res) {
 
-    auth_code = req.query.code
-    console.log(req);
+    auth_code = req.query.code;
+    //console.log("Hi" + auth_code);
     res.send('Recieved Code');
     request('https://graph.facebook.com/v2.9/oauth/access_token?client_id=1661598120549272&redirect_uri=http://localhost:8080/auth/facebook/callback&client_secret=8d57b232889eb9a213f512ab404f0c16&code='+auth_code,
         function (error, response, body) {
@@ -28,15 +27,27 @@ app.get('/auth/facebook/callback', function (req, res) {
                         return;
                     }
                     console.log('Post Id: ' + res.id);
-                    //res.send(res.id);
                 });
             }
         });
 })
 
-app.get('/schedular', function(request, response){
-    response.sendFile(path.join(__dirname +'/fbpost.html'));
-});
+app.get('/post/task', function(request, response) {
+    console.log("Hi");
+    FB.setAccessToken('EAAXnNvW8G5gBABsUKOO8pradzRfFrWZAdsclcTFahBgAJZABmyxXswWkiC9PZChJ15wc2MPQnLxKgoCkCBM62s2iZCJHCz3F2ZCUGScUYn5p2mh87pBdo6ZAoWkJnZA0hPvIyXsUok5VE91srZBZC7ZAJgUlkaaLLPEPyvv1asa76RNwZDZD');
+    var body = 'My first post my App';
+    FB.api('me/feed', 'post', {message: body}, function (res) {
+        if (!res || res.error) {
+            console.log(!res ? 'error occurred' : res.error);
+            return;
+        }
+        console.log('Post Id: ' + res.id);
+    });
+})
+
+// app.get('/schedular', function(request, response){
+//     response.sendFile(path.join(__dirname +'/fbpost.html'));
+// });
 
 app.get('/login', function(request, response){
     response.sendFile(path.join(__dirname +'/login.html'));
